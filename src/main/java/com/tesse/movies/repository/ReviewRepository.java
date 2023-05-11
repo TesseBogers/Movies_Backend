3a206eb07c2ad80451a6305357972c885d5fd4de
TesseBogers <tessebogers@hotmail.com>
2023-05-11 11:38:23 +0200

    Add Code for Backend API

---
+++ src/main/java/com/tesse/movies/repository/ReviewRepository.java
@@ -0,0 +1,12 @@
+package com.tesse.movies.repository;
+
+import com.tesse.movies.model.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
